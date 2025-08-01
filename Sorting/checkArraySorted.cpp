485474f5794e8465a7c750af71b359f45a6e94a0
Shubham Gupta <shubhamkr565@gmail.com>
2025-08-01 23:44:17 +0530

    sorting

--- Sorting/checkArraySorted.cpp
+++ Sorting/checkArraySorted.cpp
@@ -6,8 +6,18 @@ using namespace std;
 int main(){
     int arr[] = {2,4,6,8,10};
     int n = sizeof(arr)/ sizeof(arr[0]);
-
-    for (int i=0; i<n; i++){
-        
+    bool isSorted = true;
+    for (int i=0; i<n-1; i++){
+        if(arr[i] > arr[i+1]){
+            isSorted = false;
+            break;
+        }
     }
+    if(isSorted){
+        cout<<"The array is sorted in ascending order."<<endl;
+    }
+    else{
+        cout<<"The array is not sorted"<<endl;
+    }
+    return 0;
 }
