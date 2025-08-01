cedc52b9b37846b5ae767a2291742944ede7ee9a
Shubham <shubhamkr565@gmail.com>
2025-08-01 19:26:37 +0530

    sorting

---
+++ Sorting/checkArraySorted.cpp
@@ -0,0 +1,13 @@
+// Check if an Array is Sorted
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {2,4,6,8,10};
+    int n = sizeof(arr)/ sizeof(arr[0]);
+
+    for (int i=0; i<n; i++){
+        
+    }
+}
