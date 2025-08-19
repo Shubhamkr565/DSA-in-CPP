17232a31a7652799d414adc929c508437d5abc46
Shubham <shubhamkr565@gmail.com>
2025-08-19 16:54:23 +0530

    reversing2

--- LinkedList/Reversing.cpp
+++ LinkedList/Reversing.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
+<<<<<<< HEAD
 #include<vector>
 using namespace std;
 
 class Node{
     public: 
+=======
+using namespace std;
+
+class Node{
+    public:
+>>>>>>> 0e27a40 (Reversing linkedlist 2)
         int data;
         Node* next;
 
@@ -17,19 +24,32 @@ class LinkedList{
     private:
         Node* head;
         int size;
+<<<<<<< HEAD
         
+=======
+>>>>>>> 0e27a40 (Reversing linkedlist 2)
     public:
         LinkedList(){
             head = NULL;
             size = 0;
         }
+<<<<<<< HEAD
+=======
+        
+>>>>>>> 0e27a40 (Reversing linkedlist 2)
         void addLast(int data){
             Node* newNode = new Node(data);
             if(head == NULL){
                 head = newNode;
+<<<<<<< HEAD
             }else{
                 Node* temp = head;
                 while (temp->next!=NULL){
+=======
+            } else {
+                Node* temp = head;
+                while(temp->next != NULL){
+>>>>>>> 0e27a40 (Reversing linkedlist 2)
                     temp = temp->next;
                 }
                 temp->next = newNode;
@@ -37,6 +57,7 @@ class LinkedList{
             size++;
         }
 
+<<<<<<< HEAD
         void reversing(){
             if(head == NULL){
                 cout<<"Empty!";
@@ -63,10 +84,38 @@ class LinkedList{
             Node* temp = head;
 
             while(temp!=NULL){
+=======
+        void reverse(){
+            if(head == NULL){
+                cout<<"Empty list..."<<endl;
+                return;
+            }
+            Node *prev = NULL;
+            Node *curr = head;
+            Node *next = NULL;
+
+            while(curr != NULL){
+                next = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = next;
+            }
+            head = prev;
+        }
+
+        int getSize(){
+            return size;
+        }
+
+        void display(){
+            Node* temp = head;
+            while(temp != NULL){
+>>>>>>> 0e27a40 (Reversing linkedlist 2)
                 cout<<temp->data<<"->";
                 temp = temp->next;
             }
             cout<<"NULL"<<endl;
+<<<<<<< HEAD
         }
 };
 
@@ -87,3 +136,29 @@ int main(){
     L1.reversing();
     L1.display();   
 }
+=======
+            cout<<"Size: "<<getSize()<<endl;
+        }
+};
+
+int main(){
+    LinkedList l1;
+
+    l1.addLast(10);
+    l1.addLast(20);
+    l1.addLast(30);
+    l1.addLast(40);
+    l1.addLast(50);
+    l1.addLast(60);
+
+    cout<<"Original Linked List:"<<endl;
+    l1.display();
+    
+    l1.reverse();
+    
+    cout<<"After Reversing:"<<endl;
+    l1.display();
+
+    return 0;
+}
+>>>>>>> 0e27a40 (Reversing linkedlist 2)
