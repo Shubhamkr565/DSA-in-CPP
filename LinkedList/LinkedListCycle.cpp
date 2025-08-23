aa4696f8343d95f25ad192b367543fa9af0adda4
Shubham <shubhamkr565@gmail.com>
2025-08-23 22:50:22 +0530

    LinkedList Cycle

---
+++ LinkedList/LinkedListCycle.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+
+        Node(int data){
+            this->data = data;
+            this->next = NULL;
+        }
+};
+
+class LinkedList{
+    public:
+        Node* head;
+
+        LinkedList(){
+            head = NULL;
+        }
+
+        void insertAtLast(int data){
+            Node* newNode = new Node(data);
+            if(head == NULL){
+                head = newNode;
+                return;
+            }
+
+            Node* temp = head;
+            while(temp->next != NULL){
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+
+        void createCycle(int pos){
+            if(pos < 0) return;
+
+            Node* cycleNode = head;
+            for(int i = 0; i < pos && cycleNode != NULL; i++){
+                cycleNode = cycleNode->next;
+            }
+            if(cycleNode == NULL) return; // pos out of range
+
+            Node* temp = head;
+            while(temp->next != NULL){
+                temp = temp->next;
+            }
+            temp->next = cycleNode;  // connect tail to pos node
+        }
+
+        bool hasCycle(){
+            Node* slow = head;
+            Node* fast = head;
+
+            while(fast != NULL && fast->next != NULL){
+                slow = slow->next;
+                fast = fast->next->next;
+                if(slow == fast) return true;
+            }
+            return false;
+        }
+
+        void printlist(){
+            Node* temp = head;
+            int count = 0;
+            while(temp != NULL && count < 20){
+                cout<<temp->data<<"->";
+                temp = temp->next;
+                count++;
+            }
+            cout<<"NULL (stopped)"<<endl;
+        }
+};
+
+int main(){
+    LinkedList l1;
+
+    l1.insertAtLast(10);
+    l1.insertAtLast(20);
+    l1.insertAtLast(30);
+    l1.insertAtLast(40);
+    l1.insertAtLast(50);
+    l1.insertAtLast(60);
+
+    l1.printlist();
+
+    l1.createCycle(2);  // connect tail to node at index 2 (30)
+
+    cout << "Cycle present? " << (l1.hasCycle() ? "Yes" : "No") << endl;
+
+    l1.printlist(); // stops after 20 prints
+
+    return 0;
+}
