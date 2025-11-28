36a5219308da7f5563148534f5cf5ee3c1816051
Shubham <shubhamkr565@gmail.com>
2025-11-28 16:50:04 +0530

    Quadratic Problem

--- HashTable/OpenAddressing/QuadraticProbing.cpp
+++ HashTable/OpenAddressing/QuadraticProbing.cpp
@@ -11,36 +11,38 @@ class HashTable{
             table = new int[size];
 
             for(int i=0; i<size; i++){
-                table[i]= -1;
+                table[i] = -1;
             }
         }
-
-    int hashFun(int key){
-        return key%size;
-    }
-    void insertItem(int key){
-        int index = hashFun(key);
-
-        if(table[index] == -1){
-            table[index] = key;
-            return;
+    
+        int hashFun(int key){
+            return key%size;
         }
-        for(int i = 1; i<size; i++){
-            int newIndex = (index + i*i) % size;
 
-            if(table[newIndex] == -1){
-                table[newIndex] = key;
+        void insertItem(int key){
+            int index = hashFun(key);
+
+            if(table[index] == -1){
+                table[index] = key;
                 return;
             }
-        }
-        cout<<"Hash table is Full Cannot inset.\n";
-    }
+            for(int i=0; i<size; i++){
+                int newIndex = (index + i*i)%size;
 
-    bool searchingItem(int key){
-        int index = hashFun(key);
-        
-        if(table[index] == key){
-            return true;
+                if(table[newIndex] == -1){
+                    table[newIndex] = key;
+                    return;
+                }
+            }
+            cout<<"Hash Table is Full cannot insert :"<<endl;
+        }
+
+        bool searchingItem(int key){
+            int index = hashFun(key);
+
+            if(table[index] == key){
+                return true;
+            }
 
             for(int i=0; i<size; i++){
                 int newIndex = (index + i*i) % size;
@@ -48,37 +50,43 @@ class HashTable{
                 if(table[newIndex] == key){
                     return true;
                 }
-
                 if(table[newIndex] == -1){
                     return false;
                 }
             }
+            return false;
+            
         }
 
-    }
 
-    void display(){
-        for(int i=0; i<size; i++){
-            cout<<i<<" -> "<<table[i]<<endl;
+        void display(){
+            for(int i=0; i<size; i++){
+                cout<<i<<" -> "<<table[i]<<endl;
+            }
         }
-    }
 };
 
 
 
-int main(){ 
+int main(){
     HashTable h(7);
 
     h.insertItem(12);
-    h.insertItem(323);
-    h.insertItem(15464);
-    h.insertItem(56);
-    h.insertItem(27);
-    h.insertItem(674);
-
-    h.display();
-
-    h.searchingItem(27);
+    h.insertItem(14);
+    h.insertItem(16);
+    h.insertItem(121);
+    h.insertItem(19);
+    h.insertItem(78);
+    h.insertItem(91);
     
-
+    
+    
+    h.display();
+    cout<<"Searching Item: 16 "<<endl;
+    if(h.searchingItem(16)){
+        cout<<"Key Found: \n"<<endl;
+    }else{
+        cout<<"Not Found: \n"<<endl;
+    }
+    return 0; 
 }
